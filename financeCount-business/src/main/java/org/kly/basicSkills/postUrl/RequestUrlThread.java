a43539ef3eea98ff052b5c538e750bc4c40c6bb9
Colia1994 <coliakly@gmail.com>
2017-08-11 13:22:01 +0800

    添加一个模拟get post请求的投票小工具
    添加一个模拟get post请求的投票小工具

---
+++ financeCount-business/src/main/java/org/kly/basicSkills/postUrl/RequestUrlThread.java
@@ -0,0 +1,49 @@
+package org.kly.basicSkills.postUrl;
+
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * 线程开启，随机睡眠，每10次更换新的ip
+ * @author colia
+ * @date 2017-08-08
+ */
+class RequestUrlThread extends Thread {
+
+    private String url;
+    private String param;
+    private Integer currentNum = 0;
+    RequestUrlThread(String url, String param){
+        this.url = url;
+        this.param = param;
+    }
+
+    public void run(){
+        for(int j = 1; j > 0; j++) {
+            String ip = VotePostByIp.randomIpAddr("china");
+            for (int i = 0; i < 10; i++) {
+                try {
+                    long seed = System.currentTimeMillis();
+                    Random random = new Random(seed);
+                    //间隔随机秒数
+                    sleep(1000 * (random.nextInt(10) + 1));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                String sr = VotePostByIp.sendGetToUrl(url, param, ip);
+                System.out.println(sr);
+                currentNum++;
+                System.out.println("当前线程投票数为："+currentNum+"---当前系统时间："+new Date(System.currentTimeMillis()));
+            }
+            try {
+                long seed = System.currentTimeMillis();
+                Random random = new Random(seed);
+                //间隔随机秒数
+                sleep(10000 * (random.nextInt(10)+1));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
--- financeCount-business/src/main/java/org/kly/basicSkills/regular/SelectFormatString.java
+++ financeCount-business/src/main/java/org/kly/basicSkills/regular/SelectFormatString.java
@@ -1,5 +1,13 @@
 package org.kly.basicSkills.regular;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Random;
+
 /**
  * 从固定字符中筛选出目标字段
  * 测试用例：网站爬虫爬出的一堆信息，从中查出登陆人id和姓名
