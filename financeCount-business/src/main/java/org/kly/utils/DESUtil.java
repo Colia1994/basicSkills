78ced01cc59759093e93c361b9a79a177fecc0e0
Colia1994 <coliakly@gmail.com>
2019-01-21 23:39:44 +0800

    完善加密解密代码

--- financeCount-business/src/main/java/org/kly/utils/DESUtil.java
+++ financeCount-business/src/main/java/org/kly/utils/DESUtil.java
@@ -10,12 +10,15 @@ public class DESUtil {
 
 
     /**
-     * 加密
+     * 加密 解密
+     *
      * @param datasource byte[]
-     * @param password String
+     * @param password   String
+     * @param type       1 加密 2 解密
      * @return byte[]
      */
-    public static byte[] encrypt(byte[] datasource, String password) {
+    public static byte[] encrypt(byte[] datasource, String password, int type) {
+        int cipherType = type == 1 ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE;
         try {
             SecureRandom random = new SecureRandom();
             DESKeySpec desKey = new DESKeySpec(password.getBytes());
@@ -25,38 +28,14 @@ public class DESUtil {
             //Cipher对象实际完成加密操作
             Cipher cipher = Cipher.getInstance("DES/ECB/NoPadding");
             //用密匙初始化Cipher对象
-            cipher.init(Cipher.ENCRYPT_MODE, securekey, random);
-            //现在，获取数据并加密
+            cipher.init(cipherType, securekey, random);
+            //现在，获取数据并加密 ENCRYPT_MODE
             //正式执行加密操作
             return cipher.doFinal(datasource);
-        } catch(Throwable e) {
+        } catch (Throwable e) {
             e.printStackTrace();
         }
 
         return null;
-    }
-
-    /**
-     * 解密
-     * @param src byte[]
-     * @param password String
-     * @return byte[]
-     * @throws Exception
-     */
-    public static byte[] decrypt(byte[] src, String password) throws Exception {
-        // DES算法要求有一个可信任的随机数源
-        SecureRandom random = new SecureRandom();
-        // 创建一个DESKeySpec对象
-        DESKeySpec desKey = new DESKeySpec(password.getBytes());
-        // 创建一个密匙工厂
-        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
-        // 将DESKeySpec对象转换成SecretKey对象
-        SecretKey securekey = keyFactory.generateSecret(desKey);
-        // Cipher对象实际完成解密操作
-        Cipher cipher = Cipher.getInstance("DES/ECB/NoPadding");
-        // 用密匙初始化Cipher对象
-        cipher.init(Cipher.DECRYPT_MODE, securekey, random);
-        // 真正开始解密操作
-        return cipher.doFinal(src);
     }
 }
