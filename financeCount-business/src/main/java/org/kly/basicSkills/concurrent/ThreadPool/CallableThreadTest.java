b2202adb164b3776efc3c8b89f8b29af59509a18
Colia1994 <coliakly@gmail.com>
2019-03-11 17:02:35 +0800

    callable创建线程

---
+++ financeCount-business/src/main/java/org/kly/basicSkills/concurrent/ThreadPool/CallableThreadTest.java
@@ -0,0 +1,40 @@
+package org.kly.basicSkills.concurrent.ThreadPool;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @author colia
+ * @date 2019/3/11 16:55
+ */
+public class CallableThreadTest implements Callable<Integer> {
+
+
+    @Override
+    public Integer call() throws Exception {
+        int i = 0;
+        for (; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        CallableThreadTest ctt = new CallableThreadTest();
+        FutureTask<Integer> ft = new FutureTask<>(ctt);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " 的循环变量i的值" + i);
+            if (i == 20) {
+                new Thread(ft, "有返回值的线程").start();
+            }
+        }
+        try {
+            System.out.println("子线程的返回值：" + ft.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
