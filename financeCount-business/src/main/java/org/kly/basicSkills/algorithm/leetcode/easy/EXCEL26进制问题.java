62b49d7dc79e5d383923528e9f896b2f479495df
Colia1994 <coliakly@gmail.com>
2020-01-10 12:07:21 +0800

    算法演习 20200109

---
+++ financeCount-business/src/main/java/org/kly/basicSkills/algorithm/leetcode/easy/EXCEL26进制问题.java
@@ -0,0 +1,30 @@
+package org.kly.basicSkills.algorithm.leetcode.easy;
+
+/**
+ * @author colia
+ * @date 2020/1/10 12:05
+ */
+public class EXCEL26进制问题 {
+    public int titleToNumber(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int charNum = (int) s.charAt(i) - 64;
+            result =  result * 26 + charNum;
+        }
+        return result;
+
+    }
+
+    public String convertToTitle(int n) {
+        StringBuilder result = new StringBuilder();
+
+        while (n > 0) {
+            int x = n % 26;
+            x = x == 0 ? 26 : x;
+            result.insert(0, (char) (x + 64));
+            n = (n - x) / 26;
+        }
+
+        return result.toString();
+    }
+}
