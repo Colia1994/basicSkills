88243c9bc0b5d022b670014cc8d7e15736fa48de
colia <colia@foxmail.com>
2020-03-29 00:51:13 +0800

    算法演习 20200325每日一练

---
+++ financeCount-business/src/main/java/org/kly/basicSkills/algorithm/leetcode/medium/m_820_单词的压缩编码.java
@@ -0,0 +1,54 @@
+package org.kly.basicSkills.algorithm.leetcode.medium;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给定一个单词列表，我们将这个列表编码成一个索引字符串 S 与一个索引列表 A。
+ * <p>
+ * 例如，如果这个列表是 ["time", "me", "bell"]，我们就可以将其表示为 S = "time#bell#" 和 indexes = [0, 2, 5]。
+ * <p>
+ * 对于每一个索引，我们可以通过从字符串 S 中索引的位置开始读取字符串，直到 "#" 结束，来恢复我们之前的单词列表。
+ * <p>
+ * 那么成功对给定单词列表进行编码的最小字符串长度是多少呢？
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 输入: words = ["time", "me", "bell"]
+ * 输出: 10
+ * 说明: S = "time#bell#" ， indexes = [0, 2, 5] 。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= words.length <= 2000
+ * 1 <= words[i].length <= 7
+ * 每个单词都是小写字母 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/short-encoding-of-words
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author Colia
+ * @Date 2020/3/28.
+ */
+public class m_820_单词的压缩编码 {
+
+    public int minimumLengthEncoding(String[] words) {
+        Set<String> good = new HashSet(Arrays.asList(words));
+        for (String word: words) {
+            for (int k = 1; k < word.length(); ++k)
+                good.remove(word.substring(k));
+        }
+
+        int ans = 0;
+        for (String word: good)
+            ans += word.length() + 1;
+        return ans;
+    }
+
+
+}
