bdd8a79bb8e753777e21cee218cef7e1157d1c15
colia <colia@foxmail.com>
2020-04-01 21:49:07 +0800

    算法演习 20200401每日一练

---
+++ financeCount-business/src/main/java/org/kly/basicSkills/algorithm/leetcode/medium/m_1111_有效括号的嵌套深度.java
@@ -0,0 +1,17 @@
+package org.kly.basicSkills.algorithm.leetcode.medium;
+
+/**
+ * @Author Colia
+ * @Date 2020/4/1.
+ */
+public class m_1111_有效括号的嵌套深度 {
+
+    public int[] maxDepthAfterSplit(String seq) {
+        int[] ans = new int [seq.length()];
+        int idx = 0;
+        for(char c: seq.toCharArray()) {
+            ans[idx++] = c == '(' ? idx & 1 : ((idx + 1) & 1);
+        }
+        return ans;
+    }
+}
