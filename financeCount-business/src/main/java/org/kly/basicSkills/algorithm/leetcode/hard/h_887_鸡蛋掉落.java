7b6850c39149e0dbd0b063d20d3e30a855e3b015
colia <colia@foxmail.com>
2020-04-11 23:33:30 +0800

    算法演习 20200411每日一练

---
+++ financeCount-business/src/main/java/org/kly/basicSkills/algorithm/leetcode/hard/h_887_鸡蛋掉落.java
@@ -0,0 +1,29 @@
+package org.kly.basicSkills.algorithm.leetcode.hard;
+
+/**
+ * @Author Colia
+ * @Date 2020/4/11.
+ */
+public class h_887_鸡蛋掉落 {
+
+    public int superEggDrop(int K, int N) {
+        if (N == 1) {
+            return 1;
+        }
+        int[][] f = new int[N + 1][K + 1];
+        for (int i = 1; i <= K; ++i) {
+            f[1][i] = 1;
+        }
+        int ans = -1;
+        for (int i = 2; i <= N; ++i) {
+            for (int j = 1; j <= K; ++j) {
+                f[i][j] = 1 + f[i - 1][j - 1] + f[i - 1][j];
+            }
+            if (f[i][K] >= N) {
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+}
