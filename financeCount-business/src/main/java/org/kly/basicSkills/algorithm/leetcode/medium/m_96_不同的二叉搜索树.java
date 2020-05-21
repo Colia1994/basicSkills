e522638fe3bba20d25c2739ad13d00c97f893d88
konglingyao <konglingyao@meituan.com>
2020-05-21 20:57:41 +0800

    每日算法20200521

---
+++ financeCount-business/src/main/java/org/kly/basicSkills/algorithm/leetcode/medium/m_96_不同的二叉搜索树.java
@@ -0,0 +1,24 @@
+package org.kly.basicSkills.algorithm.leetcode.medium;
+
+/**
+ * @Author konglingyao
+ * @Date 2020/5/21 8:46 下午
+ */
+public class m_96_不同的二叉搜索树 {
+
+    public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+
+        return G[n];
+
+    }
+
+
+}
