d9f43ba74ac946bb453cb1fee350bb7fc57facfd
colia <colia@foxmail.com>
2020-05-26 20:00:15 +0800

    每日算法20200526

---
+++ financeCount-business/src/main/java/org/kly/basicSkills/algorithm/leetcode/medium/m_287_寻找重复数.java
@@ -0,0 +1,42 @@
+package org.kly.basicSkills.algorithm.leetcode.medium;
+
+/**
+ * 给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。假设只有一个重复的整数，找出这个重复的数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,3,4,2,2]
+ * 输出: 2
+ * 示例 2:
+ * <p>
+ * 输入: [3,1,3,4,2]
+ * 输出: 3
+ * 说明：
+ * <p>
+ * 不能更改原数组（假设数组是只读的）。
+ * 只能使用额外的 O(1) 的空间。
+ * 时间复杂度小于 O(n2) 。
+ * 数组中只有一个重复的数字，但它可能不止重复出现一次。
+ *
+ * @Author konglingyao
+ * @Date 2020/5/26 5:56 下午
+ */
+public class m_287_寻找重复数 {
+
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+
+
+
+}
