73ba9d4592204b993e9d218fdeafb501fe918485
colia <colia@foxmail.com>
2020-06-23 14:22:16 +0800

    每日算法20200623

---
+++ financeCount-business/src/main/java/org/kly/basicSkills/algorithm/leetcode/easy/e_67_二进制求和.java
@@ -0,0 +1,51 @@
+package org.kly.basicSkills.algorithm.leetcode.easy;
+
+/**
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+ * <p>
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: a = "11", b = "1"
+ * 输出: "100"
+ * 示例 2:
+ * <p>
+ * 输入: a = "1010", b = "1011"
+ * 输出: "10101"
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 每个字符串仅由字符 '0' 或 '1' 组成。
+ * 1 <= a.length, b.length <= 10^4
+ * 字符串如果不是 "0" ，就都不含前导零。
+ *
+ * @Author konglingyao
+ * @Date 2020/6/23
+ */
+public class e_67_二进制求和 {
+
+    public String addBinary(String a, String b) {
+        StringBuffer ans = new StringBuffer();
+
+        int n = Math.max(a.length(), b.length()), carry = 0;
+        for (int i = 0; i < n; ++i) {
+            carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+            carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+            ans.append((char) (carry % 2 + '0'));
+            carry /= 2;
+        }
+
+        if (carry > 0) {
+            ans.append('1');
+        }
+        ans.reverse();
+
+        return ans.toString();
+
+
+    }
+}
