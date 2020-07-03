af4c1928033c517638d4909d65693b19f9a3736a
colia <colia@foxmail.com>
2020-07-03 22:35:54 +0800

    每日算法20200703 补齐今日算法

--- financeCount-business/src/main/java/org/kly/basicSkills/algorithm/leetcode/medium/m_208_实现Trie前缀树.java
+++ financeCount-business/src/main/java/org/kly/basicSkills/algorithm/leetcode/medium/m_208_实现Trie前缀树.java
@@ -23,34 +23,69 @@ package org.kly.basicSkills.algorithm.leetcode.medium;
  */
 public class m_208_实现Trie前缀树 {
 
+    /**
+     * Your Trie object will be instantiated and called as such:
+     * Trie obj = new Trie();
+     * obj.insert(word);
+     * boolean param_2 = obj.search(word);
+     * boolean param_3 = obj.startsWith(prefix);
+     */
     class Trie {
+        private boolean is_string = false;
+        private Trie next[] = new Trie[26];
 
-        /** Initialize your data structure here. */
+        /**
+         * Initialize your data structure here.
+         */
         public Trie() {
 
         }
 
-        /** Inserts a word into the trie. */
+        /**
+         * Inserts a word into the trie.
+         */
         public void insert(String word) {
-
+            Trie root = this;
+            char[] w = word.toCharArray();
+            for (char c : w) {
+                if (root.next[c - 'a'] == null) {
+                    root.next[c - 'a'] = new Trie();
+                }
+                root = root.next[c - 'a'];
+            }
+            root.is_string = true;
         }
 
-        /** Returns if the word is in the trie. */
+        /**
+         * Returns if the word is in the trie.
+         */
         public boolean search(String word) {
-
+            char[] w = word.toCharArray();
+            Trie trie = this;
+            for (char c : w) {
+                if (trie.next[c - 'a'] == null) {
+                    return false;
+                }
+                trie = trie.next[c - 'a'];
+            }
+            return trie.is_string;
         }
 
-        /** Returns if there is any word in the trie that starts with the given prefix. */
+        /**
+         * Returns if there is any word in the trie that starts with the given prefix.
+         */
         public boolean startsWith(String prefix) {
-
+            char[] w = prefix.toCharArray();
+            Trie trie = this;
+            for (char c : w) {
+                if (trie.next[c - 'a'] == null) {
+                    return false;
+                }
+                trie = trie.next[c - 'a'];
+            }
+            return true;
         }
     }
 
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie obj = new Trie();
- * obj.insert(word);
- * boolean param_2 = obj.search(word);
- * boolean param_3 = obj.startsWith(prefix);
- */
+
 }
--- financeCount-business/src/main/java/org/kly/basicSkills/algorithm/leetcode/medium/m_662_二叉树最大宽度.java
+++ financeCount-business/src/main/java/org/kly/basicSkills/algorithm/leetcode/medium/m_662_二叉树最大宽度.java
@@ -2,6 +2,11 @@ package org.kly.basicSkills.algorithm.leetcode.medium;
 
 import org.kly.common.TreeNode;
 
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 /**
  * 给定一个二叉树，编写一个函数来获取这个树的最大宽度。树的宽度是所有层中的最大宽度。这个二叉树与满二叉树（full binary tree）结构相同，但一些节点为空。
  *
@@ -67,6 +72,52 @@ import org.kly.common.TreeNode;
 public class m_662_二叉树最大宽度 {
 
     public int widthOfBinaryTree(TreeNode root) {
-        
+        Deque<AnnotatedNode> queue = new LinkedList<>();
+        queue.add(new AnnotatedNode(root, 0, 0));
+        int curDepth = 0, left = 0, ans = 0;
+        while (!queue.isEmpty()) {
+            AnnotatedNode a = queue.poll();
+            if (a.node != null) {
+                queue.add(new AnnotatedNode(a.node.left, a.depth + 1, a.pos * 2));
+                queue.add(new AnnotatedNode(a.node.right, a.depth + 1, a.pos * 2 + 1));
+                if (curDepth != a.depth) {
+                    curDepth = a.depth;
+                    left = a.pos;
+                }
+                ans = Math.max(ans, a.pos - left + 1);
+            }
+        }
+
+        return ans;
     }
+
+    class AnnotatedNode {
+        TreeNode node;
+        int depth, pos;
+        AnnotatedNode(TreeNode n, int d, int p) {
+            node = n;
+            depth = d;
+            pos = p;
+        }
+    }
+
+    int ans;
+    Map<Integer, Integer> left;
+
+    public int widthOfBinaryTree1(TreeNode root) {
+        ans = 0;
+        left = new HashMap();
+
+        dfs(root, 0, 0);
+        return ans;
+    }
+    public void dfs(TreeNode root, int depth, int pos) {
+        if (root == null) return;
+        left.computeIfAbsent(depth, x-> pos);
+
+        ans = Math.max(ans, pos - left.get(depth) + 1);
+        dfs(root.left, depth + 1, 2 * pos);
+        dfs(root.right, depth + 1, 2 * pos + 1);
+    }
+
 }
