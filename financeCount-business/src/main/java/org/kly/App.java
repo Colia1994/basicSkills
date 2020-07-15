f81d3fba70194fd3396eedac7848e26989dc76d2
colia <colia@foxmail.com>
2020-07-15 14:58:41 +0800

    每日算法20200715

--- financeCount-business/src/main/java/org/kly/App.java
+++ financeCount-business/src/main/java/org/kly/App.java
@@ -2,6 +2,8 @@ package org.kly;
 
 import org.kly.basicSkills.algorithm.leetcode.medium.m_945_使数组唯一的最小增量;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,19 +12,16 @@ import java.util.Map;
  */
 public class App {
     public static void main(String[] args) {
-        int[] A = new int[]{3, 2, 2,2};
-
-        Map<Integer, Integer> integerMap = new HashMap<>();
-
-        //工具类的使用
-        for (Integer i : A) {
-            System.out.println("key:"+ i+",value:"+ integerMap.get(i));
-
-            integerMap.merge(i, 1,(k ,v)-> k + 1);
-            System.out.println("key:"+ i+",value:"+ integerMap.get(i));
-        }
-        integerMap.get(0);
+        String a ="abc";
+        String b = new String("abc");
+        System.out.println(a== b);
+        System.out.println(a.equals(b));
+        int a1 =1;
+        Integer b1 = new Integer(1);
+        System.out.println(a1== b1);
+        System.out.println(b1.equals(a1));
     }
+
 
 
 }
