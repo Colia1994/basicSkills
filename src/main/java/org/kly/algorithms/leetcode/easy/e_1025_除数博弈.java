8eeaa9a737e6218b28eb43be95050be4ea6d5061
colia <colia@foxmail.com>
2020-07-24 16:07:32 +0800

    每日算法20200724

---
+++ src/main/java/org/kly/algorithms/leetcode/easy/e_1025_除数博弈.java
@@ -0,0 +1,53 @@
+package org.kly.algorithms.leetcode.easy;
+
+/**
+ * 爱丽丝和鲍勃一起玩游戏，他们轮流行动。爱丽丝先手开局。
+ * <p>
+ * 最初，黑板上有一个数字 N 。在每个玩家的回合，玩家需要执行以下操作：
+ * <p>
+ * 选出任一 x，满足 0 < x < N 且 N % x == 0 。
+ * 用 N - x 替换黑板上的数字 N 。
+ * 如果玩家无法执行这些操作，就会输掉游戏。
+ * <p>
+ * 只有在爱丽丝在游戏中取得胜利时才返回 True，否则返回 false。假设两个玩家都以最佳状态参与游戏。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：2
+ * 输出：true
+ * 解释：爱丽丝选择 1，鲍勃无法进行操作。
+ * 示例 2：
+ * <p>
+ * 输入：3
+ * 输出：false
+ * 解释：爱丽丝选择 1，鲍勃也选择 1，然后爱丽丝无法进行操作。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/divisor-game
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author konglingyao
+ * @Date 2020/7/24
+ */
+public class e_1025_除数博弈 {
+
+    public boolean divisorGame(int N) {
+        boolean[] f = new boolean[N + 2];
+
+        f[1] = false;
+        f[2] = true;
+        for (int i = 3; i <= N; ++i) {
+            for (int j = 1; j < i; ++j) {
+                if ((i % j) == 0 && !f[i - j]) {
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return f[N];
+    }
+
+}
