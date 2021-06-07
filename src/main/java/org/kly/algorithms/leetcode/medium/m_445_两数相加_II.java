82c64137296702a0c48170872fa17cf4f41e266c
colia <colia@foxmail.com>
2021-06-07 21:47:28 +0800

    11

--- src/main/java/org/kly/algorithms/leetcode/medium/m_445_两数相加_II.java
+++ src/main/java/org/kly/algorithms/leetcode/medium/m_445_两数相加_II.java
@@ -11,8 +11,8 @@ import java.util.Stack;
 public class m_445_两数相加_II {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Integer> stack2 = new Stack<>();
+        Stack<Integer> stack1 = new Stack<Integer>();
+        Stack<Integer> stack2 = new Stack<Integer>();
         while (l1 != null) {
             stack1.push(l1.val);
             l1 = l1.next;
---
+++ src/main/java/org/kly/algorithms/leetcode/medium/m_494_目标和.java
@@ -0,0 +1,118 @@
+package org.kly.algorithms.leetcode.medium;
+
+/**
+ * https://leetcode-cn.com/problems/target-sum/
+ * 给你一个整数数组 nums 和一个整数 target 。
+ * <p>
+ * 向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+ * <p>
+ * 例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+ * 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,1,1,1,1], target = 3
+ * 输出：5
+ * 解释：一共有 5 种方法让最终目标和为 3 。
+ * -1 + 1 + 1 + 1 + 1 = 3
+ * +1 - 1 + 1 + 1 + 1 = 3
+ * +1 + 1 - 1 + 1 + 1 = 3
+ * +1 + 1 + 1 - 1 + 1 = 3
+ * +1 + 1 + 1 + 1 - 1 = 3
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1], target = 1
+ * 输出：1
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 20
+ * 0 <= nums[i] <= 1000
+ * 0 <= sum(nums[i]) <= 1000
+ * -1000 <= target <= 100
+ *
+ * @Author konglingyao
+ * @Date 2021/6/7
+ */
+public class m_494_目标和 {
+
+    //找到-号数回溯计算复杂度太高，合并-号的数，sum后为neg，找到合适的neg数目排列组合
+    //
+    public int findTargetSumWays(int[] nums, int target) {
+
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int diff = sum - target;
+        if (diff < 0 || diff % 2 != 0) {
+            return 0;
+        }
+        int n = nums.length, neg = diff / 2;
+        int[][] dp = new int[n + 1][neg + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            int num = nums[i - 1];
+            for (int j = 0; j <= neg; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= num) {
+                    dp[i][j] += dp[i - 1][j - num];
+                }
+            }
+        }
+        return dp[n][neg];
+    }
+
+    //错误 ，得反过来算 dpj 计算不能覆盖 dpj-num 所以需要先算j 再算j-m 见算法2
+    public int findTargetSumWays1(int[] nums, int target) {
+
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int diff = sum - target;
+        if (diff < 0 || diff % 2 != 0) {
+            return 0;
+        }
+        int neg = diff / 2;
+        int[] dp = new int[neg + 1];
+        dp[0] = 1;
+        for (int num : nums) {
+            for (int j = 0; j <= neg; j++) {
+                if (j >= num) {
+                    dp[j] += dp[j - num];
+                }
+            }
+        }
+        return dp[neg];
+    }
+
+    public int findTargetSumWay2(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int diff = sum - target;
+        if (diff < 0 || diff % 2 != 0) {
+            return 0;
+        }
+        int neg = diff / 2;
+        int[] dp = new int[neg + 1];
+        dp[0] = 1;
+        for (int num : nums) {
+            for (int j = neg; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        return dp[neg];
+    }
+
+    public static void main(String... args){
+        m_494_目标和 m1 = new m_494_目标和();
+        int[] int1 = new int[]{1,2,1};
+        System.out.println(m1.findTargetSumWays1(int1,0));
+    }
+
+
+}
