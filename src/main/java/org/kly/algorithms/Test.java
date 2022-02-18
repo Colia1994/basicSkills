b54905dcece46766a83872b92095f7df711da948
colia <colia@foxmail.com>
2022-02-18 16:24:14 +0800

    steam后聚合要保证数据之不能为空

---
+++ src/main/java/org/kly/algorithms/Test.java
@@ -0,0 +1,20 @@
+package org.kly.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author konglingyao
+ * @Date 2021/10/26
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        double l = 818076870d;
+
+        List<String> ilist = new ArrayList<>();
+        ilist.add("1");
+//        System.out.println(l % 32 );
+        System.out.println(l % 128);
+    }
+}
--- src/main/java/org/kly/javaCode/functional/Test.java
+++ src/main/java/org/kly/javaCode/functional/Test.java
@@ -1,6 +1,11 @@
 package org.kly.javaCode.functional;
 
+import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+import java.util.Objects;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * @Author konglingyao
@@ -8,28 +13,42 @@ import java.util.function.Predicate;
  */
 public class Test {
 
-    private int x= 10;
+    private int x = 10;
+
     // 使用自定义的函数式接口作为方法参数
     private static void doSomething(MyFunctionalInterface inter) {
         inter.doSomething(); // 调用自定义的函数式接口方法
     }
 
-    private static void doPredicate(Predicate<String> predicate,String str){
+    private static void doPredicate(Predicate<String> predicate, String str) {
 
         System.out.println(predicate.test(str));
     }
-    private void test01(int x){
+
+    private void test01(int x) {
         System.out.println(x);
     }
 
-    public static void main(String[] args){
-        doSomething(()->System.out.println("doing"));
-        doSomething(System.out::println);
-        doPredicate(s -> s.length() > 5 , "xxxxxxxx");
-        Test test = new Test();
-        test.test01(20);
-    }
+    public static void main(String[] args) {
+//        doSomething(() -> System.out.println("doing"));
+//        doSomething(System.out::println);
+//        doPredicate(s -> s.length() > 5, "xxxxxxxx");
+//        Test test = new Test();
+//        test.test01(20);
+//
+//        List<Long> test01 = new ArrayList<>();
+//
+//        test01.stream().collect(Collectors.toMap(p-> p, p -> false));
 
+        List<Double> metricTrendValues = new ArrayList<>();
+        metricTrendValues.add(null);
+        metricTrendValues.add(null);
+        metricTrendValues.add(null);
+        //统计分组后的总指标值
+        DoubleSummaryStatistics collect = metricTrendValues.stream().filter(Objects::nonNull)
+                .collect(Collectors.summarizingDouble(value -> value));
+        System.out.println(collect.getSum());
+    }
 
 
 }
