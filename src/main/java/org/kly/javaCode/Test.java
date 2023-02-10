879de05097cf6ff13e603b46c5614d8edd5a0bd1
colia <colia@foxmail.com>
2023-02-10 14:58:00 +0800

    自行解析yyyyMMDD格式

--- src/main/java/org/kly/javaCode/Test.java
+++ src/main/java/org/kly/javaCode/Test.java
@@ -1,5 +1,12 @@
 package org.kly.javaCode;
 
+import net.minidev.json.JSONUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @Author konglingyao
  * @Date 2023/1/5
@@ -7,31 +14,27 @@ package org.kly.javaCode;
 public class Test {
 
     public static void main(String[] args) {
-        //匿名内部类
-        Food food = new Food(){
-            //z这是 饼干
-            @Override
-            public void eat() {
-                System.out.println("被吃了");
+        String arg = "[YyMmDdHhSs]+|[^YyMmDdHhSs]";
+
+        String test01 = "YYYY-MM:DD hh:mm:ss.S";
+        Pattern pattern = Pattern.compile(arg);
+        Matcher matcher = pattern.matcher(test01);
+
+        List<String> list = new ArrayList<>();
+        while (matcher.find()) {
+            list.add(matcher.group());
+        }
+        for (String str : list) {
+            if (str.matches("[YyMmDdHhSs]+")) {
+                //这是年 月日 时分秒的map函数
+                System.out.println("map函数---"+str);
+            }else {
+                //占位符处理逻辑
+                System.out.println("占位符---" + str);
             }
-        };
-        food.eat();
-
-        Food food1 = new Org();
-        food1.eat();
+        }
 
     }
 
 }
 
-class Org implements Food{
-
-    @Override
-    public void eat() {
-        System.out.println("橘子被吃了");
-
-    }
-
-
-
-}
